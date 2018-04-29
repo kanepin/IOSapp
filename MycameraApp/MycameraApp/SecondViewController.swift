5c985ddd54241bd9c0ba150012352378750648fe
kanepin <kanepin4736@yahoo.co.jp>
2018-04-29 23:01:34 +0900

    burusukuapp

---
+++ MycameraApp/MycameraApp/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  MycameraApp
+//
+//  Created by 会社② on 2018/04/29.
+//  Copyright © 2018年 会社②. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewCotroller: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
