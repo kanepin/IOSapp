f2caf210c10acdb5d456e0172a52f0bb0faf107c
kanepin <kanepin4736@yahoo.co.jp>
2018-04-29 21:25:16 +0900

    initial commit

---
+++ Mycaeraapp/Mycaeraapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mycaeraapp
+//
+//  Created by 会社② on 2018/04/29.
+//  Copyright © 2018年 会社②. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
